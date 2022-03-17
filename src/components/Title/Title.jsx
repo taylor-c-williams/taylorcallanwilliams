06d29a7e088513012d76ba560fc19c2bac856d7c
taylor-c-williams <taylor.callan.williams@gmail.com>
2022-03-16 21:33:04 -0700

    inital header component styling

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -8,7 +8,12 @@ export default function Tile() {
 				Callan <br />
 				Williams
 			</section>
-			<section className={styles.subheader}>Software Developer</section>
+			<section className={styles.subheader}>
+				Software Developer
+			</section>
+			<section className={styles.blurb}>
+			Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
+			</section>
 		</div>
 	);
 }

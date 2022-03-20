b91cae2f950e0fea160d0d21c43425eb0b87ae34
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-03-19 20:34:13 -0700

    hamburger menu breakpoints

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,10 +3,15 @@ import styles from './Nav.module.css'
 
 export default function Nav() {
   return (
-    <div className={styles.nav}>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="#">Portfolio</NavLink>
-      <NavLink to="#">Contact</NavLink>
+    <div className={styles.navContainer}>
+      <div className={styles.hamburgerZone}>
+      <button/>
+      </div>
+      <div className={styles.navLinks}>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="#">Portfolio</NavLink>
+        <NavLink to="#">Contact</NavLink>
+      </div>
     </div>
   )
 }

682102bf42d29b25590c2eb38c40193520244077
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-03-19 21:03:46 -0700

    svg hamburger implemented

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,16 @@
-import { NavLink } from 'react-router-dom'
-import styles from './Nav.module.css'
+import { NavLink } from 'react-router-dom';
+import styles from './Nav.module.css';
 
 export default function Nav() {
   return (
     <div className={styles.navContainer}>
       <div className={styles.hamburgerZone}>
-      <button/>
+        {/* <button/> */}
+        <svg viewBox="0 0 100 80" width="30" height="30">
+          <rect width="100" height="10"></rect>
+          <rect y="30" width="100" height="10"></rect>
+          <rect y="60" width="100" height="10"></rect>
+        </svg>
       </div>
       <div className={styles.navLinks}>
         <NavLink to="/about">About</NavLink>
@@ -13,5 +18,5 @@ export default function Nav() {
         <NavLink to="#">Contact</NavLink>
       </div>
     </div>
-  )
+  );
 }

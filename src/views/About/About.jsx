5f53db67758814312388d54131f9cb72590c0fbd
Taylor Callan Williams <taylor.callan.williams@gmail.com>
2022-03-20 19:05:52 -0700

    about styles for extra-small breakpoints

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -9,16 +9,11 @@ export default function About() {
       </section>
       <header>
         <h1>Hi! I'm Taylor.</h1>
-        <span className={styles.subHeader}>I make shit for the internet.</span>
-        <span>
+        <span className={styles.subHeader}>I make stuff online.</span>
+        <span className={styles.blurb}>
           Cat ipsum dolor sit amet, human is in bath tub, emergency! drowning!
           meooowww!. Swat at dog poop on floor and watch human clean up lick the
           other cats nap all day warm up laptop with butt lick butt fart
-          rainbows until owner yells pee in litter box hiss at cats, nyan nyan
-          goes the cat, scraaaaape scraaaape goes the walls when the cat murders
-          them with its claws. Intrigued by the shower sleep nap yet toy mouse
-          squeak roll over but enslave the hooman need to petted shake treat
-          bag. X love and
         </span>
       </header>
     </div>

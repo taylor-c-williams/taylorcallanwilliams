d521133fccc010a72f2267134957ce9c3dfbfb17
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-03-22 21:23:38 -0700

    react-scroll offset correction

--- src/components/Nav/NavLinks.jsx
+++ src/components/Nav/NavLinks.jsx
@@ -5,22 +5,28 @@ export default function NavLinks() {
   return (
     <div>
       <nav className={styles.navLinks}>
-        <Link to="home" spy={true} smooth={true} offset={50} duration={500}>
+        <Link to="home" spy={true} smooth={true} offset={-100} duration={500}>
           Home
         </Link>
-        <Link to="about" spy={true} smooth={true} offset={50} duration={500}>
+        <Link to="about" spy={true} smooth={true} offset={-100} duration={500}>
           About
         </Link>
         <Link
           to="portfolio"
           spy={true}
           smooth={true}
-          offset={50}
+          offset={-100}
           duration={500}
         >
           Portfolio
         </Link>
-        <Link to="contact" spy={true} smooth={true} offset={50} duration={500}>
+        <Link
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+        >
           Contact
         </Link>
       </nav>

f83b44e4675c9e112b848a767a489aecc115db51
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-03-24 11:34:21 -0700

    xolo icon, portfolio container

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -1,20 +1,25 @@
 import { Parallax } from 'react-scroll-parallax';
 import styles from './Portfolio.module.css';
 import animations from '../Main/animations.module.css';
+import dogMatch from '../../assets/images/xoloitzcuintle.png';
+
 export default function Portfolio() {
   return (
-    <div className={styles.main}>
-      <header>
+    <div className={styles.portfolioMain}>
+      <div className={styles.portfolioContainer}>
         <Parallax speed={10}>
           <h1 className={animations.textShadowPopSl}>Portfolio</h1>
         </Parallax>
-        <span className={styles.subHeader}>I make stuff online.</span>
-        <span className={styles.blurb}>
+        <div className={styles.projectContainer}>
+          <span className={styles.subHeader}>Doggie Matcher</span>
+          <img src={dogMatch} alt="Doggie Matcher project" />
+        </div>
+        <div className={styles.blurb}>
           Cat ipsum dolor sit amet, human is in bath tub, emergency! drowning!
           meooowww!. Swat at dog poop on floor and watch human clean up lick the
           other cats nap all day warm up laptop with butt lick butt fart
-        </span>
-      </header>
+        </div>
+      </div>
     </div>
   );
 }

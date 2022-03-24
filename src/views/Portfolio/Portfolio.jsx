308171332443c571dd0543f5b8eec91f846279a0
Taylor Callan Williams <taylor.callan.williams@gmail.com>
2022-03-24 12:09:58 -0700

    portfolio component layout first draft

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -6,18 +6,25 @@ import dogMatch from '../../assets/images/xoloitzcuintle.png';
 export default function Portfolio() {
   return (
     <div className={styles.portfolioMain}>
-      <div className={styles.portfolioContainer}>
-        <Parallax speed={10}>
-          <h1 className={animations.textShadowPopSl}>Portfolio</h1>
-        </Parallax>
-        <div className={styles.projectContainer}>
-          <span className={styles.subHeader}>Doggie Matcher</span>
+      <Parallax speed={10}>
+        <h1 className={animations.textShadowPopSl}>Portfolio</h1>
+      </Parallax>
+
+      <div className={styles.projectContainer}>
+        <div className={styles.projectIcon}>
           <img src={dogMatch} alt="Doggie Matcher project" />
         </div>
-        <div className={styles.blurb}>
-          Cat ipsum dolor sit amet, human is in bath tub, emergency! drowning!
-          meooowww!. Swat at dog poop on floor and watch human clean up lick the
-          other cats nap all day warm up laptop with butt lick butt fart
+
+        <div className={styles.projectDescription}>
+          <span className={styles.subHeader}>Doggie Matcher</span>
+          <div className={styles.blurb}>
+            Cat ipsum dolor sit amet, human is in bath tub, emergency! drowning!
+            meooowww!. Swat at dog poop on floor and watch human clean up lick
+            the other cats nap all day warm up laptop with butt lick butt fart
+            <div className={styles.projectLinks}>
+              <a href="#">LIVE</a> | <a href="#">CODE</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>

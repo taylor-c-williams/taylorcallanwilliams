1373cf826de37ae09aeb93614a29e1328e2158cf
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-03-25 17:07:18 -0700

    medium viewport code links margins

--- src/views/Portfolio/MoreProjects.jsx
+++ src/views/Portfolio/MoreProjects.jsx
@@ -1,5 +1,24 @@
 import styles from './Portfolio.module.css';
 
 export default function MoreProjects() {
-  return <div className={styles.more}></div>;
+  return (
+    <div className={styles.moreMain}>
+      <div className={styles.moreProject}>
+        <h1>Project One</h1>
+        sdfkjshfkgjsrgkjndsfkjgnfdk;gndfkjgnk;jdr
+      </div>
+      <div className={styles.moreProject}>
+        <h1>Project One</h1>
+        sdfkjshfkgjsrgkjndsfkjgnfdk;gndfkjgnk;jdr
+      </div>
+      <div className={styles.moreProject}>
+        <h1>Project One</h1>
+        sdfkjshfkgjsrgkjndsfkjgnfdk;gndfkjgnk;jdr
+      </div>
+      <div className={styles.moreProject}>
+        <h1>Project One</h1>
+        sdfkjshfkgjsrgkjndsfkjgnfdk;gndfkjgnk;jdr
+      </div>
+    </div>
+  );
 }
